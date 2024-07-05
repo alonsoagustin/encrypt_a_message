729d4d9d21bbd06c1125b4bf8f9a42e5128a2292
Agustin Alonso <alonsoiagustin@gmail.com>
2024-07-05 23:32:27 +0200

    feat: :sparkles: Add request_number and request_message functions
    Asks the user for the message they want to encrypt and with what pattern.

--- functions.py
+++ functions.py
@@ -1,4 +1,34 @@
 alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ "
+number = 0
+message = ""
+
+def request_number():
+    """
+    Solicita un numero y luego devuelve dicho numero.
+    Si el usuario no ingresa un numero valido:
+        1. Devuelve ValueError.
+        2. Vuelve a solicitar un numero al usuario.
+    Si el usuario ingresa un numero valido, devuelve dicho numero.
+    """
+    global number
+    while True:
+        try:
+            number = int(input("Ingrese un numero entero: "))
+            if type(number) == float:
+                raise ValueError(f'Usted ingreso un numero con decimales: "{number}"')
+            return number
+        except ValueError as e:
+            print(f'Error: {e}. Por favor, ingrese un numero valido: ')
+
+def request_message():
+    """
+    Solicita un mensaje y luego devuelve dicho mensaje.
+    Si el usuario ingresa un mensaje vacio, la
+    funcion devuelve una cadena vacia.
+    """
+    global message
+    message = input("Ingrese el mensaje que desea encriptar: ")
+    return message
 
 def create_encrypt(positions:int):
     def encrypt(message:int)->str:
