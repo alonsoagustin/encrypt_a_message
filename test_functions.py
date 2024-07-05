eda98d83043d5775470db9def1d9e4b54e02c098
Agustin Alonso <alonsoiagustin@gmail.com>
2024-07-05 23:01:27 +0200

    test: :test_tube: Add a test for create_encrypt function.
    This new test replaces the previous test.

--- test_functions.py
+++ test_functions.py
@@ -1,8 +1,17 @@
-from functions import encrypt
+from functions import create_encrypt
 
-def test_encrypt():
-    assert encrypt("Hola mundo", 2) == "JQNCBÑWOFQ"
-    assert encrypt("ZigZag", 2) == "AKIACI"
-    assert encrypt("AKIACI", -2) == "ZIGZAG"
-    assert encrypt("ZigZag", 3) == "BLJBDJ"
-    assert encrypt("BLJBDJ", -3) == "ZIGZAG"
+def test_create_encrypt():
+    cesar3 = create_encrypt(3)
+    assert cesar3("ZigZag") == "BLJBDJ"
+
+    cesar2 = create_encrypt(2)
+    assert cesar2("Hola Mundo") == "JQNCBÑWOFQ"
+
+    cesar4 = create_encrypt(4)
+    assert cesar4("Zig Zag") == "CMKDCEK"
+
+    cesar2 = create_encrypt(2)
+    assert cesar2("ZigZag") == "AKIACI"
+
+    inverse_cesar2 = create_encrypt(-2)
+    assert inverse_cesar2("AKIACI") == "ZIGZAG"
