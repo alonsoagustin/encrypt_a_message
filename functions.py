0ee903265866caafb0208d9894436d851fc3118b
Agustin Alonso <alonsoiagustin@gmail.com>
2024-07-05 23:08:18 +0200

    feat: :sparkles: Add create_encrypt function.
    Allows you to create an encrypted message by choosing the pattern

--- functions.py
+++ functions.py
@@ -1,14 +1,16 @@
 alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ "
 
-def encrypt(message:int,positions:int)->str:
-    result = ""
-    for character in message.upper():
-        if character in alphabet:
-            i = alphabet.index(character)
-            index = i + positions
-            if index >= len(alphabet):
-                index = abs(-len(alphabet) + index)
-                result += alphabet[index]
-            else: 
-                result += alphabet[index]
-    return result
+def create_encrypt(positions:int):
+    def encrypt(message:int)->str:
+        result = ""
+        for character in message.upper():
+            if character in alphabet:
+                i = alphabet.index(character)
+                index = i + positions
+                if index >= len(alphabet):
+                    index = abs(-len(alphabet) + index)
+                    result += alphabet[index]
+                else: 
+                    result += alphabet[index]
+        return result
+    return encrypt
